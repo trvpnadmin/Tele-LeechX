4dfc6486b2f31bc9d40830cbb22ecb6ddf97a959
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-14 21:41:47 +0530

    Update & Add Theme Var

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -21,11 +21,11 @@ import telegram.ext as tg
 
 from pyrogram import Client
 
-if os.path.exists("FuZionXLogs.txt"):
-    with open("FuZionXLogs.txt", "r+") as f_d:
+if os.path.exists("FXLogs.txt"):
+    with open("FXLogs.txt", "r+") as f_d:
         f_d.truncate(0)
 
-# the logging things >>>>>>>>>>>
+# Logging Requirements >>>>>>>>>>>
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
@@ -44,10 +44,37 @@ logging.getLogger("PIL").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
 user_specific_config=dict()
+
+def getConfig(name: str):
+    return os.environ[name]
+CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL')
+try:
+    if len(CONFIG_FILE_URL) == 0:
+        raise TypeError
+    try:
+        res = requests.get(CONFIG_FILE_URL)
+        if res.status_code == 200:
+            with open('config.env', 'wb+') as f:
+                f.write(res.content)
+        else:
+            LOGGER.error(f"Failed to download config.env {res.status_code}")
+    except Exception as e:
+        LOGGER.error(f"CONFIG_FILE_URL: {e}")
+except:
+    pass
+
+try:
+    HEROKU_API_KEY = getConfig('HEROKU_API_KEY')
+    HEROKU_APP_NAME = getConfig('HEROKU_APP_NAME')
+    if len(HEROKU_API_KEY) == 0 or len(HEROKU_APP_NAME) == 0:
+        raise KeyError
+except:
+    HEROKU_APP_NAME = None
+    HEROKU_API_KEY = None
+
 dotenv.load_dotenv("config.env")
 
-# checking compulsory variable NOT NEEDED FOR OKTETO!! Just Use Your Brain
-'''
+# Compulsory Variables >>>>>>>>
 for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
     try:
         value = os.environ[imp]
@@ -56,7 +83,6 @@ for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
     except KeyError:
         LOGGER.critical(f"Oh...{imp} is missing from config.env ... fill that")
         exit()
-'''
 
 # The Telegram API things >>>>>>>>>>>
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5139162631:AAHGWHHbnbu7k5jOjXW-axcNuJcSRpBktr4")
@@ -70,7 +96,7 @@ SUDO_USERS = [int(sudos) if (' ' not in os.environ.get('SUDO_USERS', '')) else i
 AUTH_CHANNEL.append(OWNER_ID)
 AUTH_CHANNEL += SUDO_USERS
 # Download Directory >>>>>>>>>>>
-DOWNLOAD_LOCATION = "./DOWNLOADS"
+DOWNLOAD_LOCATION = "./Downloads"
 
 # Telegram maximum file upload size
 MAX_FILE_SIZE = 50000000
@@ -80,12 +106,12 @@ FREE_USER_MAX_FILE_SIZE = 50000000
 
 # chunk size that should be used with requests
 CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", "128"))
-# default thumbnail to be used in the videos
 DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/3a7f09b89943b51cdba38.jpg")
-# maximum message length in Telegram
 MAX_MESSAGE_LENGTH = 4096
-# set timeout for subprocess
+
+# Timeout for Subprocess >>>>>>>>
 PROCESS_MAX_TIMEOUT = 3600
+
 # Internal Requirements >>>>>>>>>>>
 SP_LIT_ALGO_RITH_M = os.environ.get("SP_LIT_ALGO_RITH_M", "hjs")
 ARIA_TWO_STARTED_PORT = int(os.environ.get("ARIA_TWO_STARTED_PORT", "6800"))
@@ -93,11 +119,11 @@ EDIT_SLEEP_TIME_OUT = int(os.environ.get("EDIT_SLEEP_TIME_OUT", "10"))
 MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(os.environ.get("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 600))
 MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get("MAX_TG_SPLIT_FILE_SIZE", "2097152000"))
 
-# add config vars for the display progress
+# Vars for the Display Progress >>>>>>>>
 FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "■")
 UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "□")
 
-# add offensive API
+# Add Offensive API >>>>>>>>
 TG_OFFENSIVE_API = os.environ.get("TG_OFFENSIVE_API", None)
 CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "")
 
@@ -109,34 +135,36 @@ GLEECH_COMMAND = os.environ.get("GLEECH_COMMAND", "gleech")
 GLEECH_UNZIP_COMMAND = os.environ.get("GLEECH_UNZIP_COMMAND", "gleechunzip")
 GLEECH_ZIP_COMMAND = os.environ.get("GLEECH_ZIP_COMMAND", "gleechzip")
 
-#Bot Command [ytdl] >>>>>>>>>>>
+#Bot Command [yt-dlp] >>>>>>>>>>>
 YTDL_COMMAND = os.environ.get("YTDL_COMMAND", "ytdl")
 GYTDL_COMMAND = os.environ.get("GYTDL_COMMAND", "gytdl")
+PYTDL_COMMAND = os.environ.get("PYTDL_COMMAND", "pytdl")
+GPYTDL_COMMAND = os.environ.get("GPYTDL_COMMAND", "gpytdl")
 
 #Bot Command [RClone]  >>>>>>>>>>>
 RCLONE_CONFIG = os.environ.get("RCLONE_CONFIG", "")
 DESTINATION_FOLDER = os.environ.get("DESTINATION_FOLDER", "FuZionXBot")
 INDEX_LINK = os.environ.get("INDEX_LINK", "https://covid.demonn.workers.dev/0:/FuZionXBot https://infyplexultra.mysterydemon.workers.dev/0:/FuZionXBot")
+GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize")
+CLONE_COMMAND_G = os.environ.get("CLONE_COMMAND_G", "gclone")
 TELEGRAM_LEECH_COMMAND = os.environ.get("TELEGRAM_LEECH_COMMAND", "tleech")
 TELEGRAM_LEECH_UNZIP_COMMAND = os.environ.get("TELEGRAM_LEECH_UNZIP_COMMAND", "tleechunzip")
+
+#Bot Command [Miscs]  >>>>>>>>>>>
 CANCEL_COMMAND_G = os.environ.get("CANCEL_COMMAND_G", "cancel")
-GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize")
 STATUS_COMMAND = os.environ.get("STATUS_COMMAND", "status")
 SAVE_THUMBNAIL = os.environ.get("SAVE_THUMBNAIL", "savethumb")
 CLEAR_THUMBNAIL = os.environ.get("CLEAR_THUMBNAIL", "clearthumb")
 UPLOAD_AS_DOC = os.environ.get("UPLOAD_AS_DOC", "False")
-PYTDL_COMMAND = os.environ.get("PYTDL_COMMAND", "pytdl")
-GPYTDL_COMMAND = os.environ.get("GPYTDL_COMMAND", "gpytdl")
 LOG_COMMAND = os.environ.get("LOG_COMMAND", "log")
-CLONE_COMMAND_G = os.environ.get("CLONE_COMMAND_G", "gclone")
+
+#Bot Command [Custom Bot Cmd Name]  >>>>>>>>>>>
 UPLOAD_COMMAND = os.environ.get("UPLOAD_COMMAND", "upload")
 RENEWME_COMMAND = os.environ.get("RENEWME_COMMAND", "renewme")
 RENAME_COMMAND = os.environ.get("RENAME_COMMAND", "rename")
 TOGGLE_VID = os.environ.get("TOGGLE_VID", "togglevid")
 TOGGLE_DOC = os.environ.get("TOGGLE_DOC", "toggledoc")
 RCLONE_COMMAND = os.environ.get("RCLONE_COMMAND", "rclone")
-
-#Bot Command [Utils]  >>>>>>>>>>>
 HELP_COMMAND = os.environ.get("HELP_COMMAND", "help")
 SPEEDTEST = os.environ.get("SPEEDTEST", "speedtest")
 TSEARCH_COMMAND = os.environ.get("TSEARCH_COMMAND", "tshelp")
@@ -190,6 +218,9 @@ SERVER_HOST = os.environ.get("SERVER_HOST", "Heroku")
 PRM_USERS = os.environ.get("PRM_USERS", "1242011540 503170505")
 PRM_LOG = os.environ.get("PRM_LOG", "-1001620169370")
 
+# Bot Theme [ UI & Customization ] >>>>>>>>
+BOT_THEME = os.environ.get("BOT_THEME", "FX-Optimized")
+
 BOT_START_TIME = time.time()
 # dict to control uploading and downloading
 gDict = defaultdict(lambda: [])
@@ -198,7 +229,7 @@ user_settings = defaultdict(lambda: {})
 gid_dict = defaultdict(lambda: [])
 _lock = asyncio.Lock()
 
-# Rclone Config Via any raw url
+# Rclone Config Via Any Raw URL
 ###########################################################################
 try:                                                                      #
     RCLONE_CONF_URL = os.environ.get('RCLONE_CONF_URL', "")               #
@@ -229,6 +260,7 @@ app = Client("LeechBot", bot_token=TG_BOT_TOKEN, api_id=APP_ID, api_hash=API_HAS
 if STRING_SESSION:
     userBot = Client("Tele-UserBot", api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION)
     LOGGER.info("[PRM] Initiated USERBOT") #Logging is Needed Very Much
+
 
 updater = tg.Updater(token=TG_BOT_TOKEN)
 bot = updater.bot
