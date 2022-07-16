49313272583da64ec5f0a87fb3b7665017fe602e
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-16 21:09:26 +0530

    Opps

--- tobrot/plugins/force_sub_handler.py
+++ tobrot/plugins/force_sub_handler.py
@@ -8,7 +8,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 async def get_invite_link(client, chat_id):
     try:
-        invite_link = bot.create_chat_invite_link(chat_id=chat_id)
+        invite_link = bot.create_chat_invite_link(chat_id=chat_id, member_limit=1)
         return invite_link
     except FloodWait as e:
         LOGGER.info(f"FloodWait : Sleeping {e.value}s")
@@ -27,7 +27,7 @@ async def handle_force_sub(client, cmd: Message):
         user = bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
         if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.MEMBER]:
             try:
-                invite_link = await get_invite_link(client, chat_id=channel_chat_id, member_limit=1)
+                invite_link = await get_invite_link(client, chat_id=channel_chat_id)
             except Exception as err:
                 LOGGER.info(f"Unable to Generate Invite Link of {FSUB_CHANNEL}\n\nError: {err}")
                 return 200
