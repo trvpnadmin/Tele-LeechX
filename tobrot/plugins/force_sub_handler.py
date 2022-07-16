36f020d6e37f043e097f488d405c83611c235d48
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-16 20:37:23 +0530

    Regenerate Code

--- tobrot/plugins/force_sub_handler.py
+++ tobrot/plugins/force_sub_handler.py
@@ -2,13 +2,13 @@ import asyncio
 
 from tobrot import FSUB_CHANNEL, LOGGER, bot
 from pyrogram import enums
-from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 
 async def get_invite_link(client, chat_id):
     try:
-        invite_link = await bot.create_chat_invite_link(chat_id=chat_id)
+        invite_link = bot.create_chat_invite_link(chat_id=chat_id)
         return invite_link
     except FloodWait as e:
         LOGGER.info(f"FloodWait : Sleeping {e.value}s")
@@ -25,27 +25,21 @@ async def handle_force_sub(client, cmd: Message):
         return 200
     try:
         user = bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
-        if user.status == "kicked":
-            await bot.reply_text(
-                text="**Sorry, You are Banned to Use me.**",
-                disable_web_page_preview=True
+        if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.MEMBER]:
+            try:
+                invite_link = await get_invite_link(client, chat_id=channel_chat_id)
+            except Exception as err:
+                LOGGER.info(f"Unable to Generate Invite Link of {FSUB_CHANNEL}\n\nError: {err}")
+                return 200
+            bot.send_message(
+                chat_id=cmd.chat.id,
+                text="<b>Dear,\nYou haven't joined our Channel yet.\nJoin to <u>Use Bots Without Restrictions.</u></b>",
+                parse_mode = enums.ParseMode.HTML,
+                reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("🤖 Join Channel", url=invite_link.invite_link)]
+                    ])
             )
             return 400
-    except UserNotParticipant:
-        try:
-            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
-        except Exception as err:
-            print(f"Unable to do Force Subscribe to {FSUB_CHANNEL}\n\nError: {err}")
-            return 200
-        await bot.send_message(
-            chat_id=cmd.chat.id,
-            text="**Please Join Updates Channel to use this Bot!**\n\n"
-                 "Due to Overload, Only Channel Subscribers can Use the Bot!",
-            reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)]
-                ])
-        )
-        return 400
     except Exception as err:
         LOGGER.info(f"Force Subscribe Error: {err}")
         return 200
