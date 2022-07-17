f358cf4aecb3446b705b3f39bbec94356cc5df4d
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-17 13:23:52 +0530

    Revert

--- tobrot/plugins/force_sub_handler.py
+++ tobrot/plugins/force_sub_handler.py
@@ -2,7 +2,7 @@ import asyncio
 
 from tobrot import FSUB_CHANNEL, LOGGER, bot
 from pyrogram import enums
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 
@@ -25,22 +25,28 @@ async def handle_force_sub(client, cmd: Message):
         return 200
     try:
         user = bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
-        if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.MEMBER]:
-            try:
-                invite_link = await get_invite_link(client, chat_id=channel_chat_id)
-            except Exception as err:
-                LOGGER.info(f"Unable to Generate Invite Link of {FSUB_CHANNEL}\n\nError: {err}")
-                return 200
-            bot.send_message(
-                chat_id=cmd.chat.id,
-                text="**Dear,\n__You haven't joined our Channel yet.__**\n`Join to Use Bots Without Restrictions.`",
-                reply_markup=InlineKeyboardMarkup([
-                        [
-                            InlineKeyboardButton("⚡️ Join Channel ⚡️", url=invite_link.invite_link),
-                        ]
-                    ])
+        if user.status == enums.ChatMemberStatus.BANNED or user.status == enums.ChatMemberStatus.RESTRICTED:
+            await bot.reply_text(
+                text="**Sorry, You are Banned to Use me.**",
+                disable_web_page_preview=True
             )
             return 400
+    except UserNotParticipant:
+        try:
+            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
+        except Exception as err:
+            LOGGER.info(f"Unable to Generate Invite Link of {FSUB_CHANNEL}\n\nError: {err}")
+            return 200
+        bot.send_message(
+            chat_id=cmd.chat.id,
+            text="**Dear,\n__You haven't joined our Channel yet.__**\n`Join to Use Bots Without Restrictions.`",
+            reply_markup=InlineKeyboardMarkup([
+                    [
+                        InlineKeyboardButton("⚡️ Join Channel ⚡️", url=str(invite_link.invite_link)),
+                    ]
+                ])
+        )
+        return 400
     except Exception as err:
         LOGGER.info(f"Force Subscribe Error: {err}")
         return 200
