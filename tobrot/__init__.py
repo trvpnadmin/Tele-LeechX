93020fdf0d28db4aead60ed6cb7d8f36c1d13d00
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-20 22:03:12 +0530

    Init uvloop

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -8,6 +8,7 @@
 # All Right Reserved 
 
 
+import uvloop
 import asyncio
 import logging
 import os
@@ -264,6 +265,8 @@ if not os.path.exists("rclone_bak.conf"):  # Remake and BackUp rclone.conf file
             fole.write(f.read())
     LOGGER.info("[SUCCESS] rclone.conf BackUped to rclone_bak.conf!")
 
+# Install uvloop for 2-4x Fast Asyncio 
+uvloop.install()
 
 # Pyrogram Client Intialization >>>>>>>>>>>
 app = Client("LeechBot", bot_token=TG_BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH, workers=343)
