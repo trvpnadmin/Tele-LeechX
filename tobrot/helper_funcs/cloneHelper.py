2ce98391f6eaddd61c1acd7f8d39c861832f83b9
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-25 16:12:31 +0530

    GClone Error

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -254,7 +254,7 @@ class CloneHelper:
         except IndexError:
             await asyncio.sleep(3)
             await self.lsg.delete()
-            await gcl()
+            await CloneHelper.gcl()
         except Exception as err:
             LOGGER.info(err)
             await self.lsg.edit_text(f"‼️ **ERROR** ‼️\n\n`{err}`")
